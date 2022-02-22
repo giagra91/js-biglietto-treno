2a16270e2143f2eddcf20dda9f85b41d70dee964
giagra91 <giagra91@gmail.com>
2022-02-22 15:31:46 +0100

    commenti html e modifche prezzo biglietto js

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ const age = parseFloat(prompt("Quanti anni hai?"));
 console.log(age);
 
 // Variabile per il costo del biglietto
-let ticketPrice = (distance * 0.21).toFixed(2); // toFixed(2) per limitare il prezzo del biglietto alla seconda cifra dopo la virgola
+let ticketPrice = distance * 0.21; // toFixed(2) per limitare il prezzo del biglietto alla seconda cifra dopo la virgola
 console.log(ticketPrice);
 
 // Condizioni per sconto clienti
 if (age < 18){ // condizione se il cliente è minorenne
-    ticketPrice = (ticketPrice / 100) * 80;
+    ticketPrice = ((ticketPrice / 100) * 80).toFixed(2);
 } else if (age > 65) {  // condizione se il cliente ha più di 65 anni
-    ticketPrice = (ticketPrice / 100) * 60;
+    ticketPrice = ((ticketPrice / 100) * 60).toFixed(2);
 }
 
 // Output riguardo la distanza
